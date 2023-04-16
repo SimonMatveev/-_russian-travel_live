d427453ca69ab59e4b8e05349599b4c72758a8fe
Gleb Orlovtsev <the.unknown.guest@yandex.com>
2023-04-16 19:42:27 +0300

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,89 @@
+// Проверяем что подключили скрипт и он работает
+console.log("Привет , Мир!");
+// Делаем выборку DOM элементов
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+const popupOpenButtonElement = document.querySelector('.menu__open-popup');
+
+// const introListElements = document.querySelectorAll('.intro__list-line');
+// console.log(introListElements);
+
+// introListElements.forEach(function (item, index, arr) {
+//     item.addEventListener('click', () => console.log(index))
+// })
+
+const introListElements = document.querySelector('.intro__list');
+
+// introListElements.addEventListener('click', (event) => {
+//     console.log(event.target)
+//     if(event.target.classList.contains('intro__numbers')){
+//         console.log('ПОПАЛ!');
+//     } else {
+//         console.log('НЕ ПОПАЛ!');
+//     }
+// })
+
+// const togglePopupVisibility = function (event) {
+//     console.log('togglePopupVisibility');
+//     popupElement.classList.toggle('popup_is-opened');
+// }
+
+// togglePopupVisibility()
+
+// Регистрируем обработчики события по клику
+const openPopup = function () {
+    console.log('openPopup');
+    popupElement.classList.add('popup_is-opened');
+}
+
+const closePopup = function () {
+    console.log('closePopup');
+    popupElement.classList.remove('popup_is-opened');
+}
+
+const closePopupByClickOnOverlay = function (event) {
+    // console.log('target', event.target);
+    // console.log('currentTarget', event.currentTarget);
+    if (event.target !== event.currentTarget) return;
+
+    closePopup();
+}
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+popupElement.addEventListener('click', closePopupByClickOnOverlay)
+
+// Как работают функции обратного вызова (на упрощенном примере)
+
+// const addEventListener = function (type, callback) {
+//     const event = {
+//         target: "",
+//         currentTarget: ""
+//         // ...
+//     }
+
+//     callback(event)
+
+// }
+
+const headerElement = document.querySelector('.header');
+headerElement.addEventListener('click', () => {
+    console.log('HEADER CLICKED');
+}, {
+    once: true,
+    capture: true,
+})
+
+const popupLink = popupElement.querySelector('.popup__link');
+const popupImage = popupElement.querySelector('.popup__img');
+
+const showAndHideImage = function (event) {
+    if (event.target.classList.contains("popup__link")) {
+        popupLink.classList.add("popup__link_hidden");
+        popupImage.classList.add("popup__img_visible");
+    } else if (event.target.classList.contains("popup__img")) {
+        popupLink.classList.remove("popup__link_hidden");
+        popupImage.classList.remove("popup__img_visible");
+    } else return;
+}
+
+popupElement.addEventListener('click', showAndHideImage);
